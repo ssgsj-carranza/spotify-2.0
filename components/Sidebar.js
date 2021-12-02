53999f2302b915fa47bd187e6847b899713a3767
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-12-01 20:35:56 -0600

    added styling to buttons and overall div

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,11 +2,19 @@ import {HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon} from '@heroicons/reac
 
 function Sidebar() {
     return (
-        <div>
-            
+        <div className="text-gray-500">
             <div>
-                <button>
+                <button className="flex items-center space-x-2 hover:text-white">
                     <HomeIcon className="h-5 w-5" />
+                    <p>Home</p>
+                </button>
+                <button className="flex items-center space-x-2 hover:text-white">
+                    <SearchIcon className="h-5 w-5" />
+                    <p>Search</p>
+                </button>
+                <button className="flex items-center space-x-2 hover:text-white">
+                    <LibraryIcon className="h-5 w-5" />
+                    <p>Your Library</p>
                 </button>
             </div>        
         </div>
