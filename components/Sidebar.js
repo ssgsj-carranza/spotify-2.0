259af9fc65e8e25270b78c4b92e5c2ba1087faaa
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-12-01 20:42:16 -0600

    added more styling and pulled in more icons for sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,9 +1,10 @@
-import {HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon} from '@heroicons/react/outline';
+import {HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon, HeartIcon, RssIcon} from '@heroicons/react/outline';
 
 function Sidebar() {
     return (
-        <div className="text-gray-500">
+        <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
             <div>
+                
                 <button className="flex items-center space-x-2 hover:text-white">
                     <HomeIcon className="h-5 w-5" />
                     <p>Home</p>
@@ -16,6 +17,22 @@ function Sidebar() {
                     <LibraryIcon className="h-5 w-5" />
                     <p>Your Library</p>
                 </button>
+                
+                <hr className="border-t-[0.1px] border-gray-900" />
+
+                <button className="flex items-center space-x-2 hover:text-white">
+                    <PlusCircleIcon className="h-5 w-5" />
+                    <p>Create Playlist</p>
+                </button>
+                <button className="flex items-center space-x-2 hover:text-white">
+                    <HeartIcon className="h-5 w-5" />
+                    <p>Liked Songs</p>
+                </button>
+                <button className="flex items-center space-x-2 hover:text-white">
+                    <RssIcon className="h-5 w-5" />
+                    <p>Your Episodes</p>
+                </button>
+                <hr className="border-t-[0.1px] border-gray-900" />
             </div>        
         </div>
     );
