f77ca9f9158e43e4d8d59067d6d0979836c94859
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-12-06 20:53:10 -0600

    styled sign in page, added on click sign in button, with redirect url to homepage

--- pages/login.js
+++ pages/login.js
@@ -2,12 +2,12 @@ import {getProviders, signIn} from 'next-auth/react';
 
 function Login({providers}) {
     return (
-        <div>
+        <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
             <img className="w-52 mb-5" src='https://links.papareact.com/9xl' alt=''/>
 
             {Object.values(providers).map((provider) => (
-                <div>
-                    <button>test</button>
+                <div key={provider.name}>
+                    <button onClick={() => signIn(provider.id, {callbackUrl: '/'})} className='bg-[#18D860] text-white p-5 rounded-full'>Login with {provider.name}</button>
                 </div>
             ))}        
         </div>
