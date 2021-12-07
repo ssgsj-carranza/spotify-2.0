16cdae4ac6e520f778993b6ea69da8628daa5169
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-12-06 20:48:17 -0600

    begining work on sign in page design

--- pages/login.js
+++ pages/login.js
@@ -3,7 +3,13 @@ import {getProviders, signIn} from 'next-auth/react';
 function Login({providers}) {
     return (
         <div>
-            <h1>login page</h1>        
+            <img className="w-52 mb-5" src='https://links.papareact.com/9xl' alt=''/>
+
+            {Object.values(providers).map((provider) => (
+                <div>
+                    <button>test</button>
+                </div>
+            ))}        
         </div>
     );
 }
