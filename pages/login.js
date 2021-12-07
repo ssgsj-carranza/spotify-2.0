130793a5e726700af734fcf6e0d821438b5fc77c
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-12-06 20:44:10 -0600

    set up server side rendering on log in page to render data before user sings in

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,22 @@
+import {getProviders, signIn} from 'next-auth/react';
 
-
-function Login() {
+function Login({providers}) {
     return (
         <div>
             <h1>login page</h1>        
         </div>
-    )
+    );
 }
 
 export default Login
+
+//renders it on server before login and delivers it to the client
+export async function getServerSideProps() {
+    const providers = await getProviders();
+
+    return {
+        props: {
+            providers,
+        },
+    };
+}
