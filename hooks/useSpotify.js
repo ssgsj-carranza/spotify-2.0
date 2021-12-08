072e8e44dffb5fd527a82072eda678d5a4995193
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-12-07 19:53:17 -0600

    added conditional to redirect user to login if the refresh of the access token fails

--- hooks/useSpotify.js
+++ hooks/useSpotify.js
@@ -1,15 +1,22 @@
-import { useSession } from "next-auth/react";
+import { useSession, signIn } from "next-auth/react";
 import { useEffect } from "react";
+import spotifyApi from "../lib/spotify";
 
 
 function useSpotify() {
     const {data: session} = useSession();
 
     useEffect(() => {
-        
+        if (session) {
+            //if refresh access token attempt fails, direct user to login
+            if (session.error === 'RefreshAccessTokenError') {
+                signIn();
+            }
+            spotifyApi.setAccessToken(session.user.accessToken);
+        }        
     }, [session]);
 
-    return null;
+    return spotifyApi;
 }
 
 export default useSpotify
