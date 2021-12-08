decba686787cdb6904eef99b35b9d1424063958c
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-12-07 19:23:26 -0600

    added styling to user image, continuing work on center component

--- components/Center.js
+++ components/Center.js
@@ -1,3 +1,4 @@
+import { ChevronDownIcon } from "@heroicons/react/outline";
 import { useSession } from "next-auth/react";
 
 
@@ -5,11 +6,12 @@ function Center() {
     const {data: session} = useSession();
 
     return (
-        <div className="flex flex-grow text-white">
+        <div className="flex flex-grow">
             <header>
-                <div className="">
+                <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-70 transform transition ease-in-out cursor-pointer rounded-full p-1 pr-2">
                     <img className="rounded-full w-10 h-10" src={session?.user?.image} alt="" />
                     <h2>{session?.user?.name}</h2>
+                    <ChevronDownIcon className="h-5 w-5"/>
                 </div>
             </header>       
         </div>
