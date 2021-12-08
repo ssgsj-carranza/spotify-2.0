d20d62e15c2afa0ccc6348326e2bcd88bf22a766
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-12-07 19:18:31 -0600

    pulled in user image from session as well as username from session

--- components/Center.js
+++ components/Center.js
@@ -1,9 +1,17 @@
+import { useSession } from "next-auth/react";
 
 
 function Center() {
+    const {data: session} = useSession();
+
     return (
         <div className="flex flex-grow text-white">
-            <h1>center</h1>        
+            <header>
+                <div className="">
+                    <img className="rounded-full w-10 h-10" src={session?.user?.image} alt="" />
+                    <h2>{session?.user?.name}</h2>
+                </div>
+            </header>       
         </div>
     )
 }
