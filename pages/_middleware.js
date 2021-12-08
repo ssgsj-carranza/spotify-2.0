75bc55728f0a0b69a05e36fc0dd1aad63f606323
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-12-07 18:52:16 -0600

    added conditional to middleware to allow user in if they have a token, working on logic for redirect if no token is detected

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,5 +2,15 @@ import {getToken} from 'next-auth/jwt';
 import {NextResponse} from 'next/server';
 
 export async function middleware(req) {
-    
+    //token will exist if user is logged in
+   const token = await getToken({req, secret: process.env.JWT_SECRET});
+   const {pathname} = req.nextUrl;
+   
+   //Allow requests if the following is true: 
+   //1. a request for next-auth session and provider fetching
+   if (pathname.includes('/api/auth')|| token) {
+       //basically says continue on since they have token
+       return NextResponse.next();
+   }
+   //redirect to login if they dont have a token and are requesting a protected route
 }
