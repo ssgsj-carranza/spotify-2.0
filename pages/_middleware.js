e6baa098346c37163434d2566286b0c752ffe931
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-12-07 19:02:00 -0600

    middleware working as intended

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -12,7 +12,7 @@ export async function middleware(req) {
        //basically says continue on since they have token
        return NextResponse.next();
    }
-   //redirect to login if they dont have a token and are requesting a protected route
+   //redirect to login if they dont have a token and are requesting a protected route.
    if (!token && pathname !== '/login') {
        return NextResponse.redirect('/login');
    }
