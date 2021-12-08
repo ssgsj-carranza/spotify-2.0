cfa363f9a2b3e15b0d8c553ab2f2f35f2098bc23
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-12-07 19:44:00 -0600

    created hooks folder, building a custome hook, which will hold logic for songs

--- components/Center.js
+++ components/Center.js
@@ -27,7 +27,7 @@ function Center() {
             <header className="absolute top-5 right-8">
                 <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-70 transform transition ease-in-out cursor-pointer rounded-full p-1 pr-2">
                     <img className="rounded-full w-10 h-10" src={session?.user?.image} alt="" />
-                    <h2>{session?.user?.name}</h2>
+                    <h2 className='text-white'>{session?.user?.name}</h2>
                     <ChevronDownIcon className="h-5 w-5"/>
                 </div>
             </header>
---
+++ hooks/useSpotify.js
@@ -0,0 +1,15 @@
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+
+
+function useSpotify() {
+    const {data: session} = useSession();
+
+    useEffect(() => {
+        
+    }, [session]);
+
+    return null;
+}
+
+export default useSpotify
