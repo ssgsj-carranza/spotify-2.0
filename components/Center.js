ed8426259b43b52c80d45032aecacf3c8cf89ed7
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-12-08 20:46:30 -0600

    pulled in playlist image and added some styling to it

--- components/Center.js
+++ components/Center.js
@@ -47,7 +47,7 @@ function Center() {
                 </div>
             </header>
             <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}>
-                {/* <img src="" alt="" /> */}
+                <img className='h-44 w-44 shadow-2xl' src={playlist?.images?.[0]?.url} alt="" />
             </section>       
         </div>
     )
