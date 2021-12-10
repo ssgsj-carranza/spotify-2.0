e9c25fc2fa9f6c64944c81635ff088ebf279417c
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-12-09 20:07:42 -0600

    accessed playlist state using recoil, and mapped and rendered every song on the playlist

--- components/Songs.js
+++ components/Songs.js
@@ -1,9 +1,15 @@
+import { useRecoilValue } from "recoil"
+import { playlistState } from "../atoms/playlistAtom"
 
 
 function Songs() {
+    const playlist = useRecoilValue(playlistState)
+
     return (
-        <div>
-            
+        <div className="text-white">
+            {playlist?.tracks.items.map((track) => (
+                <div>{track.track.name}</div>
+            ))}                
         </div>
     )
 }
