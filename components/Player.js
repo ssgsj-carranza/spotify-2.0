4682a817e687537d7d36f514dcb6c794c26c79f7
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-12-13 20:47:49 -0600

    created styles folder for a global button class to be applied to icons in the player component, deleted tailwind import from app and imported golabal css

--- components/Player.js
+++ components/Player.js
@@ -1,3 +1,4 @@
+import { SwitchHorizontalIcon } from "@heroicons/react/solid";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
@@ -38,15 +39,20 @@ function Player() {
     },[currentTrackId, spotifyApi, session]);
 
     return (
-        <div className='h-24 bg-gradient-to-b from-black to-gray-900 text-white'>
+        <div className='h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8'>
             {/* left section */}
-            <div>
+            <div className='flex items-center space-x-4'>
                 <img className='hidden md:inline h-10 w-10' src={songInfo?.album.images?.[0]?.url} alt="" />
                 <div>
                     <h3>{songInfo?.name}</h3>
                     <p>{songInfo?.artists?.[0]?.name}</p>
                 </div>
-            </div>        
+            </div>
+            
+            {/* center section */}
+            <div>
+                <SwitchHorizontalIcon className='button'/>        
+            </div>
         </div>
     )
 }
