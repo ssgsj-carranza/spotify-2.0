b9ee2963c6f8b8339b6a530561ba92ee949afa4c
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-12-13 20:18:13 -0600

    finished logic to fetch the current track id, as well as the access token for security measures

--- hooks/useSongInfo.js
+++ hooks/useSongInfo.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { currentTrackIdState } from "../atoms/songAtom";
 import useSpotify from "./useSpotify";
@@ -5,16 +6,30 @@ import useSpotify from "./useSpotify";
 
 //using song id to fetch song info
 function useSongInfo() {
-    const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+    const [currentIdTrack, setCurrentIdTrack] = useRecoilState(currentTrackIdState);
     const spotifyApi = useSpotify();
+    const [songInfo, setSongInfo] = useState(null, songInfo);
 
-    return (
-        <div>
-            
-        </div>
-    )
+//this is how you run an async conde inside a useEffect, by encapsulating it
+//when you make a request to an api, the access token is placed on the header, pass it around as a bearer with the token, know spotify knows the user is authenticated
+    useEffect(() => {
+        const fetchSongInfo = async () => {
+            if (currentIdTrack) {
+                const trackInfo = await 
+                fetch(`https://api.spotify.com/v1/tracks/${currentIdTrack}`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${spotifyApi.getAccessToken()}`,
+                        }
+                    }
+                ).then((res) => res.json());
+                setSongInfo(trackInfo);
+            }
+        }
+        fetchSongInfo();
+    },[currentTrackId, spotifyApi])
+
+    return songInfo;
 }
 
-export
-
-default useSongInfo
+export default useSongInfo
