e72182e4b1b20e2d6d8d37e1068a4c5b924b2969
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-12-13 21:19:00 -0600

    added onChange events on volume icons, ui working, adding useEffect to handle volume

--- components/Player.js
+++ components/Player.js
@@ -82,16 +82,16 @@ function Player() {
             </div>
             
             {/* right section */}
-            <div className='flex items-right space-x-3 md:space-x-4 justify-end pr-5'>
-                <VolumeDownIcon className='button' />
+            <div className='flex items-center space-x-3 md:space-x-4 justify-end pr-5'>
+                <VolumeDownIcon onClick={() => volume > 0 && setVolume(volume - 10)} className='button' />
                 <input className='w-14 md:w-28' 
                        type="range" 
                        value={volume} 
                        min={0} 
                        max={100}
-                       onChange={(e) = setVolume(Number(e.target.value))}
+                       onChange={(e) => setVolume(Number(e.target.value))}
                 />
-                <VolumeUpIcon className='button'/>
+                <VolumeUpIcon onClick={() => volume < 100 && setVolume(volume + 10)} className='button'/>
             </div>
         </div>
     )
